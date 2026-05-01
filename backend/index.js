6cc01ebcd112d93791ef0331acbeaebccc1b8108
Anuj Chaudhary <anujc8056@gmail.com>
2026-05-01 14:21:43 +0530

    build frontend for production

--- backend/index.js
+++ backend/index.js
@@ -2,38 +2,54 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
+
 import connectDB from "./utils/db.js";
 import userRoute from "./routes/user.route.js";
 import companyRoute from "./routes/company.route.js";
 import jobRoute from "./routes/job.route.js";
 import applicationRoute from "./routes/application.route.js";
 
-dotenv.config({});
+dotenv.config();
 
 const app = express();
 
-
-
 // middleware
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+// CORS
 const corsOptions = {
-    origin:['http://localhost:5173', "http://localhost:5174"],
-    credentials:true
-}
+  origin: ["http://localhost:5173", "http://localhost:5174"],
+  credentials: true,
+};
 app.use(cors(corsOptions));
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 
-//api's
+// API routes
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
 
 
-app.listen(PORT,()=>{ 
-    connectDB();
-    console.log(`Server running at port ${PORT}`);
-})
+// ================== FRONTEND SERVE ==================
+const __dirname = path.resolve();
+
+// static files
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+// react routing handle
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
+});
+// ====================================================
+
+
+// server start
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`Server running at port ${PORT}`);
+});
